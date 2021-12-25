c442090f537253fbbb665b8505b3114857c4e385
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-25 20:11:10 +0100

    book list parameter changes to mutable in rv book adapter

--- app/src/main/java/pl/edu/uj/reviewexchange/adapters/BookRecyclerViewAdapter.kt
+++ app/src/main/java/pl/edu/uj/reviewexchange/adapters/BookRecyclerViewAdapter.kt
@@ -2,7 +2,6 @@ package pl.edu.uj.reviewexchange.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.os.bundleOf
@@ -12,7 +11,7 @@ import pl.edu.uj.reviewexchange.R
 import pl.edu.uj.reviewexchange.databinding.RowBookBinding
 import pl.edu.uj.reviewexchange.models.Book
 
-class BookRecyclerViewAdapter(private val booksList: List<Book>)
+class BookRecyclerViewAdapter(private val booksList: MutableList<Book>)
     : RecyclerView.Adapter<BookRecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -25,20 +24,16 @@ class BookRecyclerViewAdapter(private val booksList: List<Book>)
         val book = booksList[position]
         holder.bookAuthor.text = book.author
         holder.bookName.text = book.name
-//        holder.imgViewBook.setImageResource(R.drawable.ic_book)
-//        holder.imgButtonReview.setImageResource(R.drawable.ic_review_button)
         holder.imgButtonReview.setOnClickListener(
 
-            //TODO przesylanie danych
             Navigation.createNavigateOnClickListener(
                 R.id.action_searchFragment_to_bookReviewsFragment,
-                bundleOf("book_id" to book.id, "book_name" to book.name, "book_author" to book.author)))
+                bundleOf("book_id" to book.id.toString(), "book_name" to book.name, "book_author" to book.author)))
     }
 
     override fun getItemCount(): Int = booksList.size
 
     inner class ViewHolder(binding: RowBookBinding) : RecyclerView.ViewHolder(binding.root) {
-        val imgViewBook : ImageView = binding.imageView
         val bookName : TextView = binding.tvBookRowTitle
         val bookAuthor : TextView = binding.tvBookRowAuthor
         val imgButtonReview : ImageView = binding.imageBtnBookRowReview
