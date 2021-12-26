938a2611b1cad3a0d10087df2d31ba7a87e4261a
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-26 12:10:01 +0100

    view pager 2 review display strategy logic and layout

--- app/src/main/java/pl/edu/uj/reviewexchange/fragments/ReviewDisplayStrategyVpFragment.kt
+++ app/src/main/java/pl/edu/uj/reviewexchange/fragments/ReviewDisplayStrategyVpFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import pl.edu.uj.reviewexchange.adapters.BookReviewViewPager2Adapter
 import pl.edu.uj.reviewexchange.databinding.FragmentReviewDisplayStrategyVpBinding
+import pl.edu.uj.reviewexchange.models.BookReviewPlaceHolder
 
 
 class ReviewDisplayStrategyVpFragment(private val bookId : Int) : Fragment() {
@@ -19,6 +21,8 @@ class ReviewDisplayStrategyVpFragment(private val bookId : Int) : Fragment() {
     ): View {
         _binding = FragmentReviewDisplayStrategyVpBinding.inflate(layoutInflater, container, false)
 
+        val viewpager2 = binding.vpFragmentBookDisplayStrategyVp
+        viewpager2.adapter = BookReviewViewPager2Adapter(BookReviewPlaceHolder.getBookReviews(bookId))
 
         return binding.root
     }
