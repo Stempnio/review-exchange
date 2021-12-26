ef2a7ab80df636c17b2566c5400769731e2c74bb
jakubstepien <jakub.stepien@student.uj.edu.pl>
2021-12-26 12:05:58 +0100

    added adapter for book review view pager

---
+++ app/src/main/java/pl/edu/uj/reviewexchange/adapters/BookReviewViewPager2Adapter.kt
@@ -0,0 +1,32 @@
+package pl.edu.uj.reviewexchange.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import pl.edu.uj.reviewexchange.databinding.ReviewViewpager2Binding
+import pl.edu.uj.reviewexchange.models.BookReview
+
+class BookReviewViewPager2Adapter(private val reviewList: MutableList<BookReview>)
+    : RecyclerView.Adapter<BookReviewViewPager2Adapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BookReviewViewPager2Adapter.ViewHolder {
+        return ViewHolder(ReviewViewpager2Binding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BookReviewViewPager2Adapter.ViewHolder, position: Int) {
+
+        val review = reviewList[position]
+        holder.author.text = review.userId
+        holder.review.text = review.review
+    }
+
+    override fun getItemCount(): Int = reviewList.size
+
+    inner class ViewHolder(binding : ReviewViewpager2Binding) : RecyclerView.ViewHolder(binding.root) {
+        val author = binding.tvReviewViewPager2Author
+        val review = binding.tvReviewViewPager2Review
+    }
+}
