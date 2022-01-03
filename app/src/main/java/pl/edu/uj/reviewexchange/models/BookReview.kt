07b28bcd3932042b7397c38b2f10d2c3078a7645
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-03 23:54:35 +0100

    changes bookReview model

--- app/src/main/java/pl/edu/uj/reviewexchange/models/BookReview.kt
+++ app/src/main/java/pl/edu/uj/reviewexchange/models/BookReview.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import pl.edu.uj.reviewexchange.repository.FirebaseRepository
 
-// TODO change that all
 
 data class BookReview(var id : String = "",
                       var book_id : String = "",
+                      var book_name : String = "",
                       var user_id : String = "",
-                      var review : String = "") {
-}
+                      var user_email : String = "",
+                      var review : String = "")
 
 class BookReviewViewModel : ViewModel() {
     private val repository = FirebaseRepository()
@@ -23,36 +23,3 @@ class BookReviewViewModel : ViewModel() {
         return repository.bookHasReviews(book_id)
     }
 }
-
-
-
-//object BookReviewPlaceHolder {
-//    val BOOK_REVIEWS : MutableList<BookReview> = ArrayList()
-//
-//    fun getBookReviews(bookId: Int) : MutableList<BookReview> {
-//        val reviews : MutableList<BookReview> = ArrayList()
-//        BOOK_REVIEWS.forEach {
-//            if(it.bookId == bookId)
-//                reviews.add(it)
-//        }
-//
-//        return reviews
-//    }
-//
-//
-//    init {
-//        BOOK_REVIEWS.add(BookReview().apply {
-//            this.userId = "tmp user"
-//            this.bookId = 1
-//            this.review = "asdkfjhaskjdhfkjasdasdfkjhaskldfhkjasdhfkjahsdkfjhaskjdfhkjsahdfjkashdkfjhaskjdfhaskjdhfkjasdfkjasfjkhsjkdfasjkdf"
-//        })
-//        for (i in 1..5) {
-//            BOOK_REVIEWS.add(BookReview().apply {
-//                this.userId = "user_$i"
-//                this.bookId = 1
-//                this.review = "review of book: 1, from userID: $i"
-//            })
-//        }
-//
-//    }
-//}
