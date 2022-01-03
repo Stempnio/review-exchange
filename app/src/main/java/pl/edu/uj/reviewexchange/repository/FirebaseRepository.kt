043dcc6939e0cad4d803b6c9d6abf7cf9f7b5922
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-01-03 23:56:25 +0100

    added addBook function, toast message after updating user data

--- app/src/main/java/pl/edu/uj/reviewexchange/repository/FirebaseRepository.kt
+++ app/src/main/java/pl/edu/uj/reviewexchange/repository/FirebaseRepository.kt
@@ -1,6 +1,8 @@
 package pl.edu.uj.reviewexchange.repository
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
@@ -44,30 +46,46 @@ class FirebaseRepository {
         return cloudResult
     }
 
-    fun updateUserData(data : Map<String, String>) {
+    fun updateUserData(data : Map<String, String>, context: Context) {
         cloud.collection("users")
             .document(auth.currentUser!!.uid)
             .update(data)
             .addOnSuccessListener {
                 Log.d(FIREBASE_REPOSITORY_DEBUG, "update user data SUCCESS")
+
+                Toast.makeText(context
+                    , "Data successfully updated!"
+                    , Toast.LENGTH_SHORT).show()
+
             }
-            .addOnFailureListener {
+            .addOnFailureListener { exception ->
                 Log.d(FIREBASE_REPOSITORY_DEBUG, "update user data FAIL")
+
+                Toast.makeText(context
+                    , "Error while updating data! ${exception.message.toString()}"
+                    , Toast.LENGTH_SHORT).show()
             }
     }
 
-
-    fun submitReview(review : String, book_id: String) {
+    fun submitReview(review : String, book_id: String, book_name : String, user_email : String, context: Context) {
         cloud.collection("reviews")
             .document()
             .set(mapOf("review" to review
                 ,"book_id" to book_id
+                ,"book_name" to book_name
+                ,"user_email" to user_email
                 ,"user_id" to auth.currentUser!!.uid))
             .addOnSuccessListener {
                 Log.d(FIREBASE_REPOSITORY_DEBUG, "submit review SUCCESS")
+                Toast.makeText(context
+                    , "Review successfully added!"
+                    , Toast.LENGTH_SHORT).show()
             }
-            .addOnFailureListener {
+            .addOnFailureListener { exception ->
                 Log.d(FIREBASE_REPOSITORY_DEBUG, "submit review FAIL")
+                Toast.makeText(context
+                    , "Error while submitting review! ${exception.message.toString()}"
+                    , Toast.LENGTH_SHORT).show()
             }
     }
 
@@ -144,6 +162,26 @@ class FirebaseRepository {
         return cloudResult
     }
 
+    fun getBookById(book_id: String) : LiveData<Book> {
+        val cloudResult = MutableLiveData<Book>()
+
+        cloud.collection("books")
+            .document(book_id)
+            .get()
+            .addOnSuccessListener {
+                val book = it.toObject(Book::class.java)
+                if(book != null)
+                    cloudResult.postValue(book)
+
+                Log.d(FIREBASE_REPOSITORY_DEBUG, "get book name SUCCESS")
+            }
+            .addOnFailureListener { exception ->
+                Log.d(FIREBASE_REPOSITORY_DEBUG, exception.message.toString())
+            }
+
+        return cloudResult
+    }
+
     fun getBookName(book_id: String) : LiveData<String> {
         val cloudResult = MutableLiveData<String>()
 
@@ -163,5 +201,71 @@ class FirebaseRepository {
 
         return cloudResult
     }
+
+    fun addBook(book : Book, context: Context) {
+
+        cloud.collection("books")
+            .whereEqualTo("name", book.name)
+            .get()
+            .addOnSuccessListener {
+                if(!(it.size() > 0)) {
+                    cloud.collection("books")
+                        .document(book.id)
+                        .set(mapOf("id" to book.id,
+                            "name" to book.name,
+                            "author" to book.author,
+                            "description" to book.description))
+                        .addOnSuccessListener {
+                            Log.d(FIREBASE_REPOSITORY_DEBUG, "add book SUCCESS")
+
+                            Toast.makeText(context, "Book successfully added!", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d(FIREBASE_REPOSITORY_DEBUG, "add book FAIL")
+
+                            Toast.makeText(context
+                                , "Error while adding book! ${exception.message.toString()}"
+                                , Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+    }
+
+//    fun sampleBookList() {
+//        addBook(Book.Builder()
+//            .name("Big Shot")
+//            .author("Jeff Kinney")
+//            .build())
+//
+//        addBook(Book.Builder()
+//            .name("The Judge’s List")
+//            .author("John Grisham")
+//            .build())
+//
+//        addBook(Book.Builder()
+//            .name("The Stranger In The Lifeboat")
+//            .author("Mitch Albom")
+//            .build())
+//
+//        addBook(Book.Builder()
+//            .name("The Pioneer Woman Cooks-Super Easy!")
+//            .author("Ree Drummond")
+//            .build())
+//
+//        addBook(Book.Builder()
+//            .name("The Hobbit")
+//            .author("J. R. R. Tolkien")
+//            .build())
+//
+//        addBook(Book.Builder()
+//            .name("Harry Potter and the Philosopher's Stone")
+//            .author("J. K. Rowling")
+//            .build())
+//
+//        addBook(Book.Builder()
+//            .name("The Little Prince")
+//            .author("Antoine de Saint-Exupéry")
+//            .build())
+//    }
 
 }
